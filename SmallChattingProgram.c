d055f0074f0e9f69883e168ca736496eb96652f1
Haniz <cloudstrifehan@gmail.com>
2017-06-18 19:32:18 +0700

    Update SmallChattingProgram.c

--- SmallChattingProgram.c
+++ SmallChattingProgram.c
@@ -1,112 +1,213 @@
 /*
-I write a small program to send single packet between client and server chat.
+
+I retype a small program to send single packet between client and server chat. This is full context, however, you can make this code smaller.
 It based on some docs, which I found on Internet.
 
 Reference: + http://www.geeksforgeeks.org/socket-programming-cc/
            + http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+Special thanks with:  + TCP/IP Sockets in C - Practical Guide for Programmers 
            
+ 
+
 */
 
-//======================*** SERVER *** ========================
-//============================== ** Server ** ===========================
-#include <stdio.h>
-#include <sys/socket.h>
+/* ================ This code for Server ====================*/
+#include <stdio.h>      /* for printf(), perror() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close()  */
 
-#define PORT 2003
 
-int main()  {
-  int socket_create,setsockopt, connection;
-  char buffer[1024] = {0};
-  char *msg;
-  struct sockaddr_in address;
+void DieWithError(char *errorMessage);  /* Error handling function */
+void HandleTCPClient(int clntSocket);   /* TCP client handling function */
 
-  // Socket Creation
-  if(socket_create = socket(AF_INET, SOCK_STREAM, 0) == 0){ // Create socket decriptor
-    perror("Create socket descriptor unsuccesful!\n");
+
+int main(int argc, char const *argv[]) {
+/* Declare variable */
+  int servSock;           		/* Socket descriptor for server */
+  int clntSock;         		/* Socket descriptor for client */
+  struct sockaddr_in echoServAddr;    	/* Local address */
+  struct sockaddr_in echoClntAddr;    	/* Client address */
+  unsigned short echoServPort;        	/* Server port */
+  unsigned int clntLen;               	/* Length of client address data structure */
+	
+  /* Test for correct number of argument */
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s  <Server Port>\n", argv[0]);
     exit(1);
   }
 
-  //Attaching socket to the port 2003
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons( PORT );
+  /* Get argv */
+  echoServPort = atoi(argv[1]);
 
-  //Blind to server
-  if(blind(socket_create, (const struct *) &address, sizeof(address) < 0))  {
-    perror("Blind failed!\n");
-    exit(1);
+  /* Create socket for incoming connection */
+  if ((servSock = socket(AF_INET, SOCK_STREAM,IPPROTO_TCP)) < 0){
+    DieWithError("socket() failed!");
   }
-  puts(">> ");
-  gets(msg);
+
+  /* Construct local address structure */
+  memset(&echoServAddr, 0, sizeof(echoServAddr)); 	/*set echoServerAddr zero out */
+  echoServAddr.sin_family = AF_INET;    		/* Internet family */
+  echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY);	/* any incoming interface */
+  echoServAddr.sin_port = htons(echoServPort);    	/* Local port */
+
+  /* Bind to local address */
+  if(bind(servSock , (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0) {
+    DieWithError("bind() failed");
+  }
+
+  /* Mark the socket so it will listen for incoming connections */
+  if(listen(servSock, 3) < 0) {
+    DieWithError("listen() failed");
+  }
+
   while(1)  {
-    // Listen
-    listen(socket_create, 3);
+    /* Set the size of the in-out parameter */
+    clntLen = sizeof(echoClntAddr);
 
-    // Accept any connection
-    connection = accept(socket_create, address, (socklen_t*)&addrlen);
+    /* Wait for a client to connect */
+    if((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)  {
+      DieWithError("accept() failed");
+    }
 
-    // Get packet
-    valread = read(socket_create, buffer, 1028);
-    // Print received packet
-    printf("%s\n", buffer);
-    // Send packet
-    send(socket_create, msg, strlen(msg), 0);
-    puts(">> ");
-    gets(msg);
+    /* clntSock is coonected to a client! */
+    printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
+
+    HandleTCPClient(clntSock);
   }
 
   return 0;
 }
 
+/* Declare function */
+void DieWithError(char *errorMessage) {
+  fprintf(stderr, "%s\n", errorMessage);
+  exit(1);
+}
 
-//=================================** Client ** ===============================
+void HandleTCPClient(int clntSock)  {
+  char echoBuffer[32];    /*Buffer for echo string */
 
-#include <stdio.h>
-#include <sys/socket.h>
+  char *msg = "Hello! I am Server! Your messge below: \n";		/* Message */
 
-#define PORT 2003
+  int recvMsgSize;        /* Size of received message */
 
-int main()  {
-  struct sockaddr_in address;
-  int sock = 0, valread;
-  struct sockaddr_in serv_addr;
-  char *msg, *host;
-  char buffer[1024] = {0};
+  /* Receive message from client */
+  if ((recvMsgSize = recv(clntSock, echoBuffer, 32, 0)) < 0 ) {
+    DieWithError("recv() failed");
+  }
+ 
+  while (recvMsgSize > 0) {
+    /*Echo message back to client */
+    
+    send(clntSock, msg, strlen(msg), 0);
 
-  if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)  {
-    printf("Socket creation unsuccesful!\n");
-    return -1;
+    if(send(clntSock, echoBuffer, recvMsgSize, 0) != recvMsgSize) {
+      DieWithError("send() failed");
+    }
+
+    /* See if there is more date to receive */
+    if ((recvMsgSize = recv(clntSock, echoBuffer, 32, 0)) < 0)  {
+      DieWithError("recv() failed");
+    }
+    close(clntSock); /* Close client socket() */
+  }
+}
+
+
+/* ============== This code for Client ================== */
+
+#include <stdio.h>  /* for printf() and fprintf(), perror()*/
+#include <sys/socket.h>   /* for socket(), connect(), send(), recv(), accept(),.. */
+#include <arpa/inet.h>    /* for sockaddr_in and inet_addr() */
+#include <string.h>       /* for strlen(), memset() */
+#include <unistd.h>       /* for close() */
+#include <stdlib.h>       /* for exit() */
+
+/* Declare DiewithError() function */
+void DieWithError(char *errorMessage) {
+  perror(errorMessage);
+  exit(1);
+}
+
+
+int main(int argc, char const *argv[]) {
+  /* Declare variable */
+  int sock;                 /* Socket descriptor */
+  struct sockaddr_in echoServAddr;    /* Echo server address */
+  unsigned short echoServPort;        /* Echo server port */
+  char *servIP;            /* Server IP address */
+  char *echoString;       /* String to send to echo server */
+  char echoBuffer[1024];  /* Buffer for echo string */
+  unsigned int echoStringLen;   /* Length of string to echo */
+  int bytesRcvd, totalBytesRcvd;  /*Bytes read in single recv(), and total bytes read */
+
+
+  /*  Body */
+
+  /* Check for correct input number of argument */
+  if(argc < 3 || argc > 4)  {
+    fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n", argv[0]);
+    exit(1);
   }
 
-  memset(&serv_addr, '0', sizeof(serv_addr));
+  /* Get parameter from argv */
+  servIP = argv[1];       /* First arg: server IP address (dotted quad) */
+  echoString = argv[2];    /* Second arg: string to echo */
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons( PORT );
-
-  // COnvert to text IPv4 to binary form
-  if(inet_pton(AF_INT, host, &serv_addr.sin_addr) <= 0) {
-    printf("Invalid address/ Address not supported! Converting unsuccesful!\n");
-    return -1;
+  /* Set fourth arv as echoServer Port if having 4 arg was passing */
+  if(argc == 4) {
+    echoServPort = htons(argv[3]);     /* Use given port, if any */
+  }
+  else {
+    echoServPort = htons(1997);     /* Use 1997 as port for program */
   }
 
-  //Check connection
-  if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    printf("Connection faild!\n");
-    return -1;
+
+  /* Create a reliable, stream socket using TCP */
+  if((sock = socket(AF_INET /* PF_INET */, SOCK_STREAM, IPPROTO_TCP /* 0 */)) < 0)  {
+      DieWithError("socket() failed!");
   }
 
-  // Send message!
-  send(sock, msg, strlen(msg), 0);
-  printf("Message was send!\n");
+  /* Construct the server address structure */
+  memset(&echoServAddr, 0,sizeof(echoServAddr));        /* Zero out structure */
+  echoServAddr.sin_family       = AF_INET;              /* Internet address family */
+  echoServAddr.sin_addr.s_addr  = inet_addr(servIP);    /* Server IP address */
+  echoServAddr.sin_port         = htons(echoServPort);   /* Server Port. Note: echoServPort was parse type binary before in line 39-44 */
 
-
-  // Listen and receive packet
-  while(1)  {
-    valread = read(sock, buffer, 1024);
-  }
-  while(varread != 0) {
-    printf("%s\n", buffer);
+  /* Establish the connection to the echo server */
+  if(connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)  {
+    DieWithError("Connect() failed!");
   }
 
+  /* Determine input length */
+  echoStringLen = strlen(echoString);
+
+  /* Send the string to the server */
+  if (send(sock, echoString, echoStringLen, 0) != echoStringLen)  {
+    DieWithError("send() sent a different number of bytes than expected!");
+  }
+
+  /* Receive the same string back from the server */
+  totalBytesRcvd = 0;
+  printf("Received: ");
+  while(totalBytesRcvd < echoStringLen) {
+    /* Receive up to the buffer size (minus 1 to leave space for a null terminator) */
+    if((bytesRcvd = recv(sock, echoBuffer, 1024-1, 0)) <= 0)  {
+      DieWithError("recv() failed or connection closed prematurely");
+    }
+    totalBytesRcvd += bytesRcvd;
+    echoBuffer[bytesRcvd] = '\0'; /* Null byte for terminal message */
+    printf("%s", echoBuffer);
+  }
+
+  printf("\n");
+
+  /* Close socket  and finish program */
+  close(sock);
   return 0;
 }
+
+
